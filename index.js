f009486d1227927085dc448c0cf543d6e293be33
Luke Carrier <luke@carrier.im>
2020-06-06 02:27:22 +0100

    Begin fleshing out exporter
    This code is heavily based on the upstream exporters, but I've tried to
    DRY it up as best I can. There are a lot of missing features right now,
    which I'll raise issues for.

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
+const path = require('path');
+
+const puppeteer = require('puppeteer');
+
+const EXPORT_URL = 'file://' + path.join(__dirname, 'export.html');
+const RESULT_INFO_SELECTOR = '#result-info';
 const FORMATS = [
 ];
+
+async function launchExporter(timeout=30000) {
+  console.debug('Launching browser via Puppeteer');
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--disable-gpu',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+    ],
+  });
+
+  console.debug('Preparing a new page');
+  const page = await browser.newPage();
+  page.on('console', message => console.debug('Browser:', message.text()));
+
+  console.debug('Navigating to the exporter');
+  await page.goto(EXPORT_URL, {
+    waitUntil: 'networkidle0',
+  });
+
+  console.debug(`Setting up browser timeout in ${timeout} microseconds`);
+  const browserTimeout = setTimeout(() => {
+    console.warn('Closing browser from timeout');
+    browser.close();
+  }, timeout);
+
+  return {browser, browserTimeout, page};
+}
 
 async function exportDiagram(input, pageIndex, format) {
   input = input.toString();
